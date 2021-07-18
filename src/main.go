2e11b7c4f53b1836d5f42d02f2502bfee1328ccd
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-07-18 11:29:42 +0900

    fix: help

--- src/main.go
+++ src/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 var RootCmd = &cobra.Command {
-	Use: "SNSを使う",
-	Short: "Programer only SNS のクライアント",
-	Run: func(cmd *cobra.Command, args [] string) {
+	Use: "Programer only SNS client",
+	Short: "Programer only SNS のクライアント。メッセージを送信/受信します、",
+	Run: func(cmd *cobra.Command, args []string){
 		fmt.Println("Root command")
 	},
 }
