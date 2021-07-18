d66c87349e164bbc1593de8436b5595be58281d0
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-07-18 10:58:39 +0900

    impl: root command

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/javaboy-github/only-programer-sns-client
+
+go 1.16
+
+require (
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/spf13/cobra v0.0.3 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"github.com/spf13/cobra"
 )
 
+var RootCmd = &cobra.Command {
+	Use: "SNSを使う",
+	Short: "Programer only SNS のクライアント",
+	Run: func(cmd *cobra.Command, args [] string) {
+		fmt.Println("Root command")
+	},
+}
+
 func main() {
-	fmt.Println("Hello, world")
+    if err := RootCmd.Execute(); err != nil {
+        fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+        os.Exit(-1)
+    }
 }
