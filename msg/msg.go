b5e27bc14e6f49743e4838147d5aca82068d5909
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-07-25 14:24:35 +0900

    ref: be fast

--- msg/msg.go
+++ msg/msg.go
@@ -75,46 +75,80 @@ func sendMsgCmd() *cobra.Command {
 	return cmd
 }
 
+// キャッシュを更新する。
+// @param newData 新しいデータ。このデータを元に更新する。また、データが足りない場合は重い処理になる。
+func updateMsgs(newData []map[string]string) (int, []map[string]string) {
+	fmt.Println("キャッシュを更新します...")
+	// IDを取り出す
+	ids:= []string{}
+	for _, e := range newData {
+		ids= append(ids, e["id"])
+	}
+
+	// キャッシュを読み込む
+	{
+		cacheFile, err := os.OpenFile("text-datas.json", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+		if err != nil {
+			// キャッシュがない場合->更新
+			goto updateAllData
+		}
+
+		cache := []map[string]string{}
+		content, _ := ioutil.ReadAll(cacheFile)
+		json.Unmarshal([]byte(content), cache)
+		// キャッシュ内を検索し、補完できる場合は補完する。
+		for i, e := range cache {
+			if e["id"] == newData[0]["id"] {
+				// 補完できる
+				// 補完
+				for j, v := range newData {
+					v["number"] = strconv.Itoa(i + j)
+					cache[i + j] = v
+				}
+				// 保存
+				jsonCacheData  , _ := json.Marshal(cache)
+				cacheFile.Write([]byte(jsonCacheData))
+				defer cacheFile.Close()
+				return i, cache
+			}
+		}
+		defer cacheFile.Close()
+		// 補完できない場合はそのまま下記が実行される
+	}
+
+updateAllData:
+	// データをすべて更新
+	resp, _ := http.Get("https://versatileapi.herokuapp.com/api/text/all")
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	var result []map[string]string
+	json.Unmarshal([]byte(body), &result)
+	// キャッシュの更新
+	file, err := os.OpenFile("text-datas.json", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		color.Red("キャッシュの更新に失敗!")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for i, e := range result {
+		e["number"] = strconv.Itoa(i)
+	}
+	jsonCacheData  , _ := json.Marshal(result)
+	file.Write(jsonCacheData)
+	defer file.Close()
+		for i, e := range result {
+			if e["id"] == newData[0]["id"] {
+				return i, result
+}}
+return -1, nil
+}
+
 func seeMsgsCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "すべてのメッセージを表示し、キャッシュを作成します",
 		Run: func(cmd *cobra.Command, args []string) {
-			// ユーザーのリストを取得
-			userList := user.ReadUsers()
-
-			// メッセージ一覧を取得
-			resp, _ := http.Get("https://versatileapi.herokuapp.com/api/text/all")
-			defer resp.Body.Close()
-			body, _ := io.ReadAll(resp.Body)
-			var result []map[string]string
-			n := 0 // 匿名ナンバー
-			json.Unmarshal([]byte(body), &result)
-			texts := map[string][]string{} // リプライ実装用
-			// キャッシュの更新
-			file, err := os.OpenFile("text-datas.json", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
-			if err != nil {
-				color.Red("キャッシュの更新に失敗!")
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			cacheData := []string{}
-			for _, e := range result {
-				cacheData = append(cacheData, e["id"])
-			}
-			jsonCacheData  , _ := json.Marshal(cacheData)
-			file.Write(jsonCacheData)
-			defer file.Close()
-
-			// テキストIDを追加
-			for i, e := range result {
-				e["text_id"] = fmt.Sprint(i)
-			}
-			// 降順にする
-			for i := 0; i < len(result)/2; i++ {
-				result[i], result[len(result)-i-1] = result[len(result)-i-1], result[i]
-			}
-			// ページとリミット分だけ表示する
+			// フラグを取得
 			limit, err1 := cmd.Flags().GetInt("limit")
 			page, err2 := cmd.Flags().GetInt("page")
 			if err1 != nil {
@@ -126,18 +160,44 @@ func seeMsgsCmd() *cobra.Command {
 			if err1 != nil || err2 != nil {
 				os.Exit(1)
 			}
-			result = result[limit*page : limit*page+limit]
+			// ユーザーのリストを取得
+			userList := user.ReadUsers()
+
+			// メッセージ一覧を取得
+			resp, _ := http.Get(fmt.Sprintf("https://versatileapi.herokuapp.com/api/text/all?$orderby=_created_at+desc&$skip=%d&$limit=%d", page * limit, limit))
+			defer resp.Body.Close()
+			body, _ := io.ReadAll(resp.Body)
+			var result []map[string]string
+			n := 0 // 匿名ナンバー
+			json.Unmarshal([]byte(body), &result)
+
 			// 順番を戻す
 			for   i := 0; i < len(result)/2; i++ {
 				result[i], result[len(result)-i-1] = result[len(result)-i-1], result[i]
 			}
+			// キャッシュの更新
+			index, texts := updateMsgs(result)
+			getElement := func(id string) map[string]string {
+				for _, e := range texts {
+					if e["id"] == id {
+						return e
+					}
+				}
+				return nil
+			}
+			if index == -1 {
+				color.Red("キャッシュの更新でエラー!")
+			}
 
+			// テキストIDを追加
+			for i, e := range result {
+				e["text_id"] = fmt.Sprint(index + i)
+			}
 			for _, e := range result {
 				if _, ok := userList[e["_user_id"]]; !ok {
 					userList[e["_user_id"]] = "不明" + strconv.Itoa(n)
 					n++
 				}
-				texts[e["id"]] = []string{e["_user_id"], e["text"]}
 				// リプライに関する文字列
 				reply := ""
 				replayToText, ok1 := e["in_reply_to_text_id"]
@@ -145,33 +205,34 @@ func seeMsgsCmd() *cobra.Command {
 				if ok1 && ok2 {
 					// ユーザーとテキストにリプライ
 					// リプライ先のユーザー名とリプライ先のテキストのユーザー名が同じ場合
-					val, err := texts[replayToText]
-					if !err {
-						reply = "不明>"
-					} else if val[0] == replayToUser {
-						reply = fmt.Sprintf("%s %s>", color.BlueString("@"+userList[replayToUser]), texts[replayToText][1])
+					val := getElement(replayToText)
+					if val == nil {
+							reply = "不明>"
+					} else
+					if val["_user_id"] == replayToUser {
+						reply = fmt.Sprintf("%s %s>", color.BlueString("@"+userList[replayToUser]),getElement(replayToText)["text"])
 					} else {
 						// ただの地獄
-						reply = fmt.Sprintf("%s,%s %s>", color.BlueString("@"+userList[replayToUser]), color.BlueString("@"+userList[texts[replayToText][0]]), texts[replayToText][1])
+						reply = fmt.Sprintf("%s,%s %s>", color.BlueString("@"+userList[replayToUser]), color.BlueString("@"+userList[getElement(replayToText)["name"]]), getElement(replayToText)["text"])
 					}
 				} else if ok2 {
 					// ユーザーのみにリプライ
 					reply = fmt.Sprintf("%s>", color.BlueString("@"+userList[replayToUser]))
 				} else if ok1 {
 					// テキストのみにリプライ
-					val, ok := texts[replayToText]
-					if !ok {
-						val = []string{"不明", "不明"}
-					}
-					reply = fmt.Sprintf("%s %s>", color.BlueString("@"+userList[val[0]]), val[1])
-				}
+					val := getElement(replayToText)
+					if val == nil {
+						reply = "不明 不明>"
+					}else{
+					reply = fmt.Sprintf("%s %s>", color.BlueString("@"+userList[val["name"]]), val["text"])
+				}}
 				date, _ := time.Parse("2006-01-02T15:04:05.000+00:00", e["_created_at"])
 				fmt.Printf("%s[#%s][%s] %s\n", color.BlueString(userList[e["_user_id"]]), color.GreenString(e["text_id"]), color.YellowString(date.In(time.FixedZone("Asia/Tokyo", 9*60*60)).Format("2006-01-02 15:04:05")), reply)
 				fmt.Println(e["text"])
 			}
 		},
 	}
-	cmd.Flags().IntP("limit", "l", 20, "人ページに表示する数")
+	cmd.Flags().IntP("limit", "l", 20, "1ページに表示する数")
 	cmd.Flags().IntP("page", "p", 0, "表示するページ")
 	return cmd
 }
